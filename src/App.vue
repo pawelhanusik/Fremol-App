f1f8d4d5bc4d56d5e79cc7b9e29dbf5be5d283c9
Pawel Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2021-03-03 10:54:04 +0100

    Move some methods from MainLayout.vue to App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,56 @@
   </div>
 </template>
 <script>
+
+let unsubscibe = null
+
 export default {
-  name: 'App'
+  name: 'App',
+  beforeMount() {
+    // set axios & laravel echo connection
+    this.$store.dispatch('server/setAxiosBaseURL')
+    this.$store.dispatch('server/setEchoHostOptions')
+    this.$store.dispatch('server/setAccessToken')
+    // check connection to server
+    this.$store.dispatch('server/checkApiConnection')
+    // fetch user data & conversations
+    this.$store.dispatch('user/fetchUserData')
+    this.$store.dispatch('conversations/fetchConversations')
+  },
+  created() {
+    unsubscibe = this.$store.subscribe((mutation, state) => {
+      // update axios & laravel echo connection
+      if (
+        mutation.type == 'server/SET_HOST'
+        || mutation.type == 'server/SET_APIPORT'
+        || mutation.type == 'server/SET_WSPORT'
+      ) {
+        this.$store.dispatch('server/setAxiosBaseURL')
+        this.$store.dispatch('server/setEchoHostOptions')
+      }
+
+      // update axios token
+      else if (mutation.type == 'server/SET_TOKEN') {
+        this.$store.dispatch('server/setAccessToken')
+      }
+      
+      // redirect on server connected/disconnected
+      else if (
+        mutation.type == 'server/SET_ISCONNECTED'
+      ) {
+        if (mutation.payload === true) {
+          this.$router.push('/')
+        } else {
+          this.$router.push('/settings')
+          this.$q.notify('Cannot connect. Check server settings')
+        }
+      }
+    })
+  },
+  destroyed() {
+    if (unsubscibe !== null) {
+      unsubscibe()
+    }
+  },
 }
 </script>
