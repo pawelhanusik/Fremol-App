07af77d30739025e423ab8c985784f3acce9e94a
Pawel Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2021-05-05 13:05:27 +0200

    Remove unnecessary code

--- src-capacitor/android/app/src/main/java/pl/hanusik/pawel/fremol/MainActivity.java
+++ src-capacitor/android/app/src/main/java/pl/hanusik/pawel/fremol/MainActivity.java
@@ -17,11 +17,5 @@ public class MainActivity extends BridgeActivity {
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
     }});
-
-    /*if (BuildConfig.DEBUG) {
-      EnableHttpsSelfSigned.enable(this.bridge);
-    }*/
-
-
   }
 }
